32ce6d61bec869017378423a0b6ed93148ea530e
Luciano De Carolis <parodia452@gmail.com>
2023-05-24 16:10:18 -0300

    delete cors from app.js & added scripts start

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config(); //.config() es para que lea las variables declaras en el archivo .env
-const cors = require('cors');
+// const cors = require('cors');
 const Server = require('./models/server');
 
 const server = new Server();
